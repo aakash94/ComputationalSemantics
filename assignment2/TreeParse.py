ad6f16442b2e84d963b391fb54299ad967da6153
Aakash Maroti <aakash.the.limitless@gmail.com>
2021-10-29 19:50:28 +0200

    Update TreeParse.py
    - Add context
    - Dump TSV
    - Randomly sampled values

--- assignment2/TreeParse.py
+++ assignment2/TreeParse.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from io import open
 from conllu import parse_incr
 
@@ -19,6 +20,8 @@ class TreeParse:
         :type conllu_path: str
         '''
         self.conllu_data = open(conllu_path, "r", encoding="utf-8")
+        self.header = ['context_before', 'target_sentence', 'context_after']
+        self.df = pd.DataFrame(columns=self.header)
 
     def print_all_lines(self):
         '''
@@ -29,7 +32,33 @@ class TreeParse:
         for sentence in parse_incr(self.conllu_data):
             print(sentence.metadata['text'])
 
-    def get_lines(self, word="also", max_count=128, compare_lowercase=True, compare_title=True, compare_uppercase=True):
+    def remove_tabs(self, strng):
+        '''
+
+        :param strng: the input string from which all tab characters must be removed.
+        :type strng:str
+        :return: returns the same string with all the tabs replaced to space
+        :rtype: str
+        '''
+        return strng.replace("\t", " ")
+
+    def get_context_lines(self, list7):
+        '''
+        :param list7:
+        :type list7:list
+        :return: returns 1st 3 and last 3 sentences from the token list without any tabs
+        :rtype: tuple
+        '''
+        lines_before = list7[0].metadata['text'] + " " + list7[1].metadata['text'] + " " + list7[2].metadata['text']
+        lines_after = list7[4].metadata['text'] + " " + list7[5].metadata['text'] + " " + list7[6].metadata['text']
+        return self.remove_tabs(lines_before), self.remove_tabs(lines_after)
+
+    def get_lines(self,
+                  word="also",
+                  max_count=1024,
+                  compare_lowercase=True,
+                  compare_title=True,
+                  compare_uppercase=True):
         '''
         :param word: The word to be searched in the sentence
         :type word: str
@@ -41,51 +70,95 @@ class TreeParse:
         :type compare_title: bool
         :param compare_uppercase: True if the uppercase version of the original word should be searched
         :type compare_uppercase: bool
-        :return: List of strings, where each string is a line in the conllu file that contains the word
-        :rtype: list
+        :return: returns a dataframe which has target sentence and and surrounding context
+        :rtype: dataframe
         '''
+        context_before_list = []
         sentence_list = []
+        context_after_list = []
+
+        last7 = []
 
         for sentence in parse_incr(self.conllu_data):
+
+            last7.append(sentence)
+
+            if len(last7) < 7:
+                continue
+
+            elif len(last7) > 7:
+                last7.pop(0)
+
             if len(sentence_list) == max_count:
                 break
 
-            if sentence.filter(form=word).__len__() > 0:
-                sentence_list.append(sentence.metadata['text'])
+            target_sentence = last7[3]
 
-            elif compare_lowercase and sentence.filter(form=word.lower()).__len__() > 0:
-                sentence_list.append(sentence.metadata['text'])
+            if target_sentence.filter(form=word).__len__() > 0:
+                l_before, l_after = self.get_context_lines(last7)
+                sentence_string = self.remove_tabs(target_sentence.metadata['text'])
+                sentence_list.append(sentence_string)
+                context_before_list.append(l_before)
+                context_after_list.append(l_after)
 
-            elif compare_title and sentence.filter(form=word.title()).__len__() > 0:
-                sentence_list.append(sentence.metadata['text'])
+            elif compare_lowercase and target_sentence.filter(form=word.lower()).__len__() > 0:
+                l_before, l_after = self.get_context_lines(last7)
+                sentence_string = self.remove_tabs(target_sentence.metadata['text'])
+                sentence_list.append(sentence_string)
+                context_before_list.append(l_before)
+                context_after_list.append(l_after)
 
-            elif compare_uppercase and sentence.filter(form=word.upper()).__len__() > 0:
-                sentence_list.append(sentence.metadata['text'])
+            elif compare_title and target_sentence.filter(form=word.title()).__len__() > 0:
+                l_before, l_after = self.get_context_lines(last7)
+                sentence_string = self.remove_tabs(target_sentence.metadata['text'])
+                sentence_list.append(sentence_string)
+                context_before_list.append(l_before)
+                context_after_list.append(l_after)
 
-        return sentence_list
+            elif compare_uppercase and target_sentence.filter(form=word.upper()).__len__() > 0:
+                l_before, l_after = self.get_context_lines(last7)
+                sentence_string = self.remove_tabs(target_sentence.metadata['text'])
+                sentence_list.append(sentence_string)
+                context_before_list.append(l_before)
+                context_after_list.append(l_after)
+
+        self.df['context_before'] = context_before_list
+        self.df['target_sentence'] = sentence_list
+        self.df['context_after'] = context_after_list
+
+        return self.df
+
+    def dump_tsv(self, dump_path, max_count=128):
+        '''
+
+        :param dump_path: the path where the file will be dumped
+        :type dump_path: str
+        :param max_count: how many random samples to dump
+        :type max_count: int
+        :return: none
+        :rtype: none
+        '''
+        df_count = len(self.df.index)
+        sample_count = min(max_count, df_count)
+        sampled_lines = self.df.sample(sample_count)
+        sampled_lines.to_csv(dump_path, sep="\t", index=False)
+        print("Dumped ", sample_count, " lines.")
 
 
 def demo():
     print("Hello World, this function demonstrates how to use TreeParse")
     print("install conllu to use this script. use 'pip install conllu'")
+    print("The output csv is TAB separated and NOT comma separated")
     print("All the best!\n\n\n")
 
     CONLLU_PATH = "data/Universal Dependencies 2.8.1/ud-treebanks-v2.8/UD_English-GUM/en_gum-ud-test.conllu"
     WORD = "peppermint"
+    OUTPUT_PATH = "data/output.csv"
 
     # To use, create an instance of the class, and pass the path to the CONLLU file
     tp = TreeParse(conllu_path=CONLLU_PATH)
-
-    # To view all the lines in the file
-    # tp.print_all_lines()
-
-    # To get lines containing the word, simply pass the word to the get_lines word.
-    sentence_list = tp.get_lines(word=WORD)
-
-    # You can do whatever required with the lines here.
-    print(len(sentence_list), " Sentences found for the word ", WORD)
-    for s in sentence_list:
-        print(s)
+    tp.get_lines(word=WORD)
+    tp.dump_tsv(dump_path=OUTPUT_PATH)
 
 
 if __name__ == "__main__":
