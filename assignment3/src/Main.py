cc5dfd59b786bb9721657b08869baeddcbcaf42f
Aakash Maroti <aakash.the.limitless@gmail.com>
2021-12-25 05:07:19 +0100

    Update Main.py

--- assignment3/src/Main.py
+++ assignment3/src/Main.py
@@ -17,12 +17,25 @@ def load_dicts(preprocessed_path):
     return text_d, parents_d
 
 
+def get_parents(tweet_id, parents_dict):
+    parents = []
+    x = parents_dict[tweet_id]
+    while x != '':
+        parents.insert(0,x)
+        x = parents_dict[x]
+
+    return parents
+
+
 def main():
     print("Hello World!\nWelcome to Assignment3\nSDQC\n")
-    preprocessed_path =  os.path.join("..","res", "pre_processed")
-    text_d , parents_d = load_dicts(preprocessed_path=preprocessed_path)
+    preprocessed_path = os.path.join("..", "res", "pre_processed")
+    text_d, parents_d = load_dicts(preprocessed_path=preprocessed_path)
     print(len(text_d))
     print(len(parents_d))
+
+    parents = get_parents("580341529870348288", parents_d)
+    print(parents)
 
 
 if __name__ == "__main__":
